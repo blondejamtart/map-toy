bec9fa0fdf26c0f0f555a7166e66119798bf7a77
Bryan T <blondejamtart@gmail.com>
2021-03-02 17:37:17 +0000

    begin dealing with map transform

--- laskowski.py
+++ laskowski.py
@@ -1,11 +1,18 @@
 from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D 
+
 from PIL import Image
 import numpy
 
-sz = 101
-hf = 50
+sz_x = 101
+sz_y = 101
 
 im = Image.open('/home/falcon/Downloads/wrld-11/mercrator-grey.png')
+merc = numpy.array(im)
+print("Image is %d by %d (by %d)" % merc.shape)
+
+sz_x = merc.shape[1]
+sz_y = merc.shape[0]
 
 # stole this from d3-geo-projections
 def transformPoint(lam, phi):
@@ -16,21 +23,27 @@ def transformPoint(lam, phi):
 	return pt
 
 # l -> -pi to pi, p -> -pi/2 to pi/2 (polar co-ords on surface)
-l = (numpy.array(range(sz)) - hf) * numpy.pi * (2.0/(sz-1))
-p = (numpy.array(range(sz)) - hf) * numpy.pi * (1.0/(sz-1))
+l = numpy.linspace(-numpy.pi, numpy.pi, sz_x)
+p = numpy.linspace(-0.5*numpy.pi, 0.5*numpy.pi, sz_y)
 
-grdX = numpy.zeros((sz,sz))
-grdY = numpy.zeros((sz,sz))
 
-for i in range(sz):
-	for j in range(sz):
+grdX = numpy.zeros((sz_x,sz_y))
+grdY = numpy.zeros((sz_x,sz_y))
+
+for i in range(sz_x):
+	for j in range(sz_y):
 		grdX[i,j], grdY[i,j] = transformPoint(l[i], p[j])
 
 fig = pyplot.figure()
-for i in range(0,sz, 10):
-	pyplot.plot(grdX[:,i], grdY[:,i])
-	pyplot.plot(grdX[i,:], grdY[i,:])
+ax = fig.add_subplot(111, projection='3d')
+
+for i in range(0,sz_y, 10):
+	ax.scatter(grdX[:,i], grdY[:,i], merc[i,:,1])
+
+#for i in range(0,sz_y, 10):
+#	pyplot.plot(grdX[:,i], grdY[:,i])
+#for i in range(0,sz_x, 10):
+#	pyplot.plot(grdX[i,:], grdY[i,:])
 
 pyplot.show()
 
-merc = numpy.array(im)
