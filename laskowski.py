5e00b8b2ddbbc6cb5239c3d60d241207b5a36095
Bryan T <blondejamtart@gmail.com>
2021-03-02 19:19:50 +0000

    tweak reprojection

--- laskowski.py
+++ laskowski.py
@@ -3,6 +3,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 from PIL import Image
 import numpy
+import math
 
 sz_x = 101
 sz_y = 101
@@ -16,11 +17,11 @@ sz_y = merc.shape[0]
 
 # stole this from d3-geo-projections
 def transformPoint(lam, phi):
-	pt = [
-		lam * (0.975534 + phi**2 * (-0.119161 + lam**2 * -0.0143059 + phi**2 * -0.0547009)),
-		phi * (1.00384 + lam**2 * (0.0802894 + phi**2 * -0.02855 + lam**2 * 0.000199025) + phi**2 * (0.0998909 + phi**2 * -0.0491032))
-	]
-	return pt
+    pt = [
+        lam * (0.975534 + phi**2 * (-0.119161 + lam**2 * -0.0143059 + phi**2 * -0.0547009)),
+        phi * (1.00384 + lam**2 * (0.0802894 + phi**2 * -0.02855 + lam**2 * 0.000199025) + phi**2 * (0.0998909 + phi**2 * -0.0491032))
+    ]
+    return pt
 
 # l -> -pi to pi, p -> -pi/2 to pi/2 (polar co-ords on surface)
 l = numpy.linspace(-numpy.pi, numpy.pi, sz_x)
@@ -29,21 +30,41 @@ p = numpy.linspace(-0.5*numpy.pi, 0.5*numpy.pi, sz_y)
 
 grdX = numpy.zeros((sz_x,sz_y))
 grdY = numpy.zeros((sz_x,sz_y))
+err = numpy.zeros((sz_x,sz_y))
+hitCount = numpy.zeros((sz_x,sz_y))
+reproj = numpy.zeros((sz_x,sz_y))
 
 for i in range(sz_x):
-	for j in range(sz_y):
-		grdX[i,j], grdY[i,j] = transformPoint(l[i], p[j])
+    for j in range(sz_y):
+        grdX[i,j], grdY[i,j] = transformPoint(l[i], p[j])
+
+xRange = grdX.max() - grdX.min()
+yRange = grdY.max() - grdY.min()
+
+grdX = (grdX - grdX.min())*((sz_x-1)/xRange)
+grdY = (grdY - grdY.min())*((sz_y-1)/yRange)
+
+for i in range(sz_x):
+    for j in range(sz_y):
+        err[i,j] = math.sqrt(abs(grdX[i,j] - round(grdX[i,j]))**2 + abs(grdY[i,j] - round(grdY[i,j]))**2)
+        hitCount[round(grdX[i,j]),round(grdY[i,j])] += 1
+        reproj[round(grdX[i,j]),round(grdY[i,j])]=numpy.sum(merc[round(grdY[i,j]),round(grdX[i,j]),:3])
 
 fig = pyplot.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-for i in range(0,sz_y, 10):
-	ax.scatter(grdX[:,i], grdY[:,i], merc[i,:,1])
+#ax = fig.add_subplot(111, projection='3d')
 
 #for i in range(0,sz_y, 10):
-#	pyplot.plot(grdX[:,i], grdY[:,i])
-#for i in range(0,sz_x, 10):
-#	pyplot.plot(grdX[i,:], grdY[i,:])
+#    ax.scatter(grdX[:,i], grdY[:,i], merc[i,:,1])
 
+for i in range(0,sz_y, 100):
+    pyplot.plot(grdX[:,i], grdY[:,i])
+for i in range(0,sz_x, 100):
+    pyplot.plot(grdX[i,:], grdY[i,:])
+
+pyplot.show()
+#pyplot.imshow(err)
+#pyplot.show()
+#pyplot.imshow(hitCount > 0)
+pyplot.imshow(reproj)
 pyplot.show()
 
